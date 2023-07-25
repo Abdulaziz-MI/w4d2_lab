98c6c802c149b8bc7f14c78ba677dbcec358b7ba
Abdulaziz-MI <am.alzaid@outlook.com>
2023-07-25 21:45:01 +0100

    Created properties and methods for Passenger class

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,45 @@
 public class Passenger {
+    private static int passengerCount = 0;
+    private String name;
+    private String email;
+    private int id;
+
+
+
+
+    public Passenger(){
+        this.name = "";
+        this.email= "";
+        this.id = ++passengerCount;
+
+    }
+
+    public static int getPassengerCount() {
+        return passengerCount;
+    }
+
+    public static void setPassengerCount(int passengerCount) {
+        Passenger.passengerCount = passengerCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
 }
